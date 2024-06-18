7adbdc81bdb1d403b8cd1b74564dbfef12448339
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2024-06-18 00:08:08 -0600

    Update all.go

--- backend/all/all.go
+++ backend/all/all.go
@@ -3,7 +3,7 @@ package all
 
 import (
 	// Active file systems
-	_ "github.com/RArtutos/gclone-SAS/backend/drive"
+	_ "github.com/dogbutcat/gclone/backend/drive"
 	_ "github.com/rclone/rclone/backend/alias"
 	_ "github.com/rclone/rclone/backend/azureblob"
 	_ "github.com/rclone/rclone/backend/azurefiles"
